d1481cd36ab380817541f3e127dffcc7d0e033f3
Ricky-yu <ts00484515@gmail.com>
2023-01-19 23:48:40 +0900

    create strategy

--- strategy/strategy.dart
+++ strategy/strategy.dart
@@ -1,1 +1,41 @@
+abstract class Strategy {
+  int execute(List<int> values);
+}
 
+class SumStrategy implements Strategy {
+  @override
+  int execute(List<int> values) {
+    return values.reduce((value1, value2) => value1 + value2);
+  }
+}
+
+class MultiplyStrategy implements Strategy {
+  @override
+  int execute(List<int> values) {
+    return values.reduce((value1, value2) => value1 * value2);
+  }
+}
+
+class Sequence {
+  List<int> numbers;
+
+  Sequence(this.numbers);
+
+  void addNumber(int value) {
+    numbers.add(value);
+  }
+
+  int compute(Strategy strategy) {
+    return strategy.execute(numbers);
+  }
+}
+
+void main() {
+  Sequence sequence = Sequence([1, 2, 3, 4, 5]);
+
+  Strategy sumStrategy = SumStrategy();
+  print("Sum: ${sequence.compute(sumStrategy)}");
+
+  Strategy multiplyStrategy = MultiplyStrategy();
+  print("Multiply: ${sequence.compute(multiplyStrategy)}");
+}
