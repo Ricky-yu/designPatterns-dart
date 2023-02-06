eabb60b52ab40cb1c8b2b9bcbebd1479bd1cd77b
Ricky-yu <ts00484515@gmail.com>
2023-02-06 23:00:25 +0900

    add iterator

--- iterator/iterator.dart
+++ iterator/iterator.dart
@@ -1,1 +1,26 @@
+class ColorsIterator implements Iterator {
+  var _colors = [
+    "Red",
+    "Orange",
+    "Yellow",
+    "Green",
+    "Blue",
+    "Indigo",
+    "Violet"
+  ];
 
+  var _index = 0;
+
+  @override
+  get current => _colors[_index++];
+
+  @override
+  bool moveNext() => _index < _colors.length;
+}
+
+void main() {
+  final colors = ColorsIterator();
+  while (colors.moveNext()) {
+    print(colors.current);
+  }
+}
